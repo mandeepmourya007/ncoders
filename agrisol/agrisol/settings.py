7943c9ed2d1365f147f67faaf94658ead5c63cfb
root <root@localhost.localdomain>
2019-03-08 16:29:55 +0530

    models customer

--- agrisol/agrisol/settings.py
+++ agrisol/agrisol/settings.py
@@ -37,6 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'accounts',
+    'cust_sell',
+    'farmer',
+    'helpline',
+    'household',
 ]
 
 MIDDLEWARE = [
--- agrisol/cust_sell/models.py
+++ agrisol/cust_sell/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
 # Create your models here.
+class seller(models.Model):
+    name = models.CharField(max_length=25)
+    rate = models.IntegerField(max_length=25)
+    images = models.FileField(null=True, blank=True, upload_to="static/events/images")
+    state = models.CharField(max_length=25)
+    city = models.CharField(max_length=25)
+
+    state = (('Punjab', 'Punjab'),
+           ('Delhi', 'Delhi'),
+           ('Bihar', 'Bihar'),
+           ('UP', 'UP'),
+             ('Orissa', 'Orissa'),
+           ('Haryana', 'Haryana'),
+           ('Meghalaya', 'Meghalaya'),
+
+           )
+
+
+class customer(models.Model):
+    name = models.CharField(max_length=25)
+    #place = models.IntegerField(max_length=25)
+    mobile_number = models.CharField(max_length=10)
+
+
+
