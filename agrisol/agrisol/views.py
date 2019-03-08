4d58979bb3b19882e1b2fd87d7b50c60b032f428
root <root@localhost.localdomain>
2019-03-09 03:46:06 +0530

    a

--- agrisol/agrisol/views.py
+++ agrisol/agrisol/views.py
@@ -5,6 +5,7 @@ from django.contrib.auth.decorators import login_required
 from django.conf import settings
 from django.contrib.auth.models import User
 
+
 def home(request):
     return render(request,"home.html")
 
--- agrisol/household/views.py
+++ agrisol/household/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
 
 # Create your views here.
+def household(request):
+    return render(request,'household.html')
